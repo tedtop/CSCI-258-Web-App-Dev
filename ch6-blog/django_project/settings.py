064d9e3b8fbdf1440ae8eadcab75ba1d924df434
tedtop <tedtoporkov@gmail.com>
2025-11-28 16:39:07 -0700

    Ch6 Blog & Ch 7,8 Auth

--- ch6-blog/django_project/settings.py
+++ ch6-blog/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'blog',
 ]
 
@@ -123,3 +124,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login/Logout redirect
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
